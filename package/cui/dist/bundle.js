47732c334703d1932d802139cce38b2529d10589
rjsdnql123 <rjsdnql123@gmail.com>
2023-12-22 21:33:41 +0900

    First commit

---
+++ package/cui/dist/bundle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+var Hello = function Hello() {
+  return /*#__PURE__*/React.createElement("div", null, "Hello World!");
+};
+
+export { Hello };
+//# sourceMappingURL=bundle.js.map
