5ac2e97f329a4559e51f72f8bda22255e5d5578a
rjsdnql123 <rjsdnql123@gmail.com>
2023-12-23 22:53:16 +0900

    (Feat): story book setting

--- package/cui/dist/bundle.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-var Hello = function Hello() {
-  return /*#__PURE__*/React.createElement("div", null, "Hello World!");
-};
-
-export { Hello };
-//# sourceMappingURL=bundle.js.map
