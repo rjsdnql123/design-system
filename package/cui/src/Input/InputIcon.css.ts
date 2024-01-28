b27ea5012f4cc35a6eeb40b505b569119c5d0398
rjsdnql123 <rjsdnql123@gmail.com>
2024-01-29 01:29:51 +0900

    (Fix) : css 수정

--- package/cui/src/Input/InputIcon.css.ts
+++ package/cui/src/Input/InputIcon.css.ts
@@ -16,30 +16,31 @@ const inputIcon = style({
   //   pointerEvents: clickable ? 'auto' : 'none',
 });
 
-const inputIconSvg = style({
-  selectors: {
-    [`${inputIcon} :global(svg)`]: {
-      //   width: `calc(${vars.inputHeight} - 2px)`,
-      //   height: `calc(${vars.inputHeight} - 2px)`,
-      transform: 'scale(0.44)',
-    },
-  },
-});
+// const inputIconSvg = style({
+//   selectors: {
+//     [`${inputIcon} :global(svg)`]: {
+//       //   width: `calc(${vars.inputHeight} - 2px)`,
+//       //   height: `calc(${vars.inputHeight} - 2px)`,
+//       transform: 'scale(0.44)',
+//     },
+//   },
+// });
 
-const inputInner = style({
-  //   width: "100%",
-  //   padding: `110`,
-  //   border: "none",
-  //   ":focus": {
-  //     outline: "none",
-  //   },
-  // selectors: {
-  //   ':focus': {
-  //     outline: 'none',
-  //   },
-  //   ':disabled': {
-  //     backgroundColor: 'unset',
-  //   },
-  // },
-});
+// const inputInner = style({
+//   //   width: "100%",
+//   //   padding: `110`,
+//   //   border: "none",
+//   //   ":focus": {
+//   //     outline: "none",
+//   //   },
+//   // selectors: {
+//   //   ':focus': {
+//   //     outline: 'none',
+//   //   },
+//   //   ':disabled': {
+//   //     backgroundColor: 'unset',
+//   //   },
+//   // },
+// });
+
 export { inputIcon };
