f568ba2244db4bf72e3cf5e6c99b54dc2bd6e272
rjsdnql123 <rjsdnql123@gmail.com>
2024-01-29 00:40:01 +0900

    (test): 배포 테스트

--- package/monorepo-test-app/src/app/page.tsx
+++ package/monorepo-test-app/src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from '@monorepo/cui';
 import { Radio } from '@monorepo/cui';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
       <Button>dd</Button>
       <Radio />
+      <Link href={'/test'} > go test</Link>
     </main>
   );
 }
