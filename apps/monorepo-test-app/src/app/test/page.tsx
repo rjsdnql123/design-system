705182ca980ffc3fbb13df8f6015842be9effce9
rjsdnql123 <rjsdnql123@gmail.com>
2024-01-29 23:45:32 +0900

    (Test): test page

--- apps/monorepo-test-app/src/app/test/page.tsx
+++ apps/monorepo-test-app/src/app/test/page.tsx
@@ -1,7 +1,17 @@
-export default function Test() {
+async function getProjects() {
+  return new Promise((res, rej) => {
+    res('123123')
+  })
+}
+
+export default async function  Test() {
+  const a = await getProjects()
     return (
       <main>
         qwkejqlwkejqwlke
+        <div>
+          {a}
+        </div>
       </main>
     );
   }
