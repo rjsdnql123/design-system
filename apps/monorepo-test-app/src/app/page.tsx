67f41b8e245867f53a0b2a0ef858e7fab38a5cc9
rjsdnql123 <rjsdnql123@gmail.com>
2024-01-30 20:52:09 +0900

    (Test): test page

--- apps/monorepo-test-app/src/app/page.tsx
+++ apps/monorepo-test-app/src/app/page.tsx
@@ -1,13 +1,18 @@
+import { createRandomId } from '@monorepo/common';
 import { Button } from '@monorepo/cui';
 import { Radio } from '@monorepo/cui';
 import Link from 'next/link';
+import { useId } from 'react';
 
 export default function Home() {
+  const asdf = createRandomId();
+  console.log(asdf)
   return (
     <main>
       <Button>dd</Button>
+      {asdf}
       <Radio />
-      <Link href={'https://design-system-monorepo-test-app.vercel.app/test'} > go test</Link>
+      <Link href={`https://design-system-monorepo-test-app.vercel.app/test/${asdf}`} > go test</Link>
     </main>
   );
 }
