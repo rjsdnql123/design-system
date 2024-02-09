f97a0fed3e825d0c2ec46b224773b604e37eae8e
rjsdnql123 <rjsdnql123@gmail.com>
2024-02-09 22:12:52 +0900

    (Fix): shadowDOM Tag 수정

--- package/cui/src/Components/ShadowDom/ShadowDom.stories.tsx
+++ package/cui/src/Components/ShadowDom/ShadowDom.stories.tsx
@@ -15,7 +15,7 @@ const CountShadowDom: React.FC = () => {
       <div>
         쉐도우 돔이 아닙니다.
       </div>
-      <ShadowDom css='div {color: blue;} span {background: red;};'>
+      <ShadowDom css='div {color: blue;} * {background: red;};'>
         쉐도우 돔 안에 있는 children
         <span>dd</span>
       </ShadowDom>
